b8859dd631284bbb634f00ec6c019c017c5b11a4
Jakub Krzyzanowski <jakub.krzyzanowski@nokia.com>
2024-07-26 22:09:11 +0200

    more plugins

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- Bootstrap lazy.nvim
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -6,7 +7,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -20,4 +21,3 @@ vim.g.maplocalleader = "\\"
 
 require("vim-options")
 require("lazy").setup("plugins")
-
